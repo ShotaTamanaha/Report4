bb984dea37ccbb071c69fef36bdb485ae3c9bd2c
Shita Tamanaha <e195751@ie.u-ryukyu.ac.jp>
2019-11-30 18:24:23 +0900

    step2

--- src/main/java/Report4.java
+++ src/main/java/Report4.java
@@ -1,10 +1,14 @@
+import java.io.IOException;
+
 public class Report4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String str;
         str = null;
-        System.out.println(str.length());
-
-
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {//ここでキャッチして標準出力する
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(e.getMessage());
+        }
     }
-
 }
