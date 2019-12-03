8d29871272a7d333f3e365c17dc1c0ca1333efc6
Shita Tamanaha <e195751@ie.u-ryukyu.ac.jp>
2019-12-03 17:37:50 +0900

    step3

--- src/main/java/Report4.java
+++ src/main/java/Report4.java
@@ -2,8 +2,13 @@ import java.io.IOException;
 
 public class Report4 {
     public static void main(String[] args) {
-       String str = "百二十三";
-       int value = Integer.parseInt(str);
-       System.out.println(value);
+       try {
+           String str = "百二十三";
+        int value = Integer.parseInt(str);
+       System.out.println(value);}
+       catch (NumberFormatException e){
+           System.out.println("NumberFormatExceptionのエラーが発生した");
+           System.out.println(e.getMessage());
+       }
     }
 }
